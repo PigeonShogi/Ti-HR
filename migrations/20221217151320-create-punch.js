7e445ed26cd996aba354a3deae04f63c52566181
Ckai <ckai.trans@gmail.com>
2022-12-17 23:19:35 +0800

    feat: add punch model

---
+++ migrations/20221217151320-create-punch.js
@@ -0,0 +1,37 @@
+'use strict'
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('punches', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      working_day: {
+        type: Sequelize.DATEONLY
+      },
+      state: {
+        type: Sequelize.STRING
+      },
+      in: {
+        type: Sequelize.DATE
+      },
+      out: {
+        type: Sequelize.DATE
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('punches')
+  }
+}
---
+++ models/punch.js
@@ -0,0 +1,28 @@
+'use strict'
+const {
+  Model
+} = require('sequelize')
+module.exports = (sequelize, DataTypes) => {
+  class punch extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate (models) {
+      // define association here
+    }
+  }
+  punch.init({
+    workingDay: DataTypes.DATEONLY,
+    state: DataTypes.STRING,
+    in: DataTypes.DATE,
+    out: DataTypes.DATE
+  }, {
+    sequelize,
+    modelName: 'Punch',
+    tableName: 'Punches',
+    underscored: true
+  })
+  return punch
+}
