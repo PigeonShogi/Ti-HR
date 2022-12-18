9756d61877ce6fceaa1e8e4f570d0dfa89406487
Ckai <ckai.trans@gmail.com>
2022-12-18 11:19:44 +0800

    feat: set data association

--- models/punch.js
+++ models/punch.js
@@ -3,17 +3,14 @@ const {
   Model
 } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
-  class punch extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+  class Punch extends Model {
     static associate (models) {
-      // define association here
+      Punch.belongsTo(models.Employee, {
+        foreignKey: 'EmployeeId'
+      })
     }
   }
-  punch.init({
+  Punch.init({
     workingDay: DataTypes.DATEONLY,
     state: DataTypes.STRING,
     in: DataTypes.DATE,
@@ -25,5 +22,5 @@ module.exports = (sequelize, DataTypes) => {
     tableName: 'Punches',
     underscored: true
   })
-  return punch
+  return Punch
 }
