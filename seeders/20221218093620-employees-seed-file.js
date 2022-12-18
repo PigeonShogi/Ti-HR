8948f770515f5d861134fd6ce8f739ec558a9dda
Ckai <ckai.trans@gmail.com>
2022-12-18 18:32:55 +0800

    feat: add employees-seed-file

---
+++ seeders/20221218093620-employees-seed-file.js
@@ -0,0 +1,18 @@
+'use strict'
+const employeeData = require('./employee-data.json')
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Employees',
+      Array.from(employeeData, value => ({
+        code: value.code,
+        full_name: value.full_name,
+        created_at: new Date(),
+        updated_at: new Date()
+      })
+      ), {})
+  },
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Employees', null, {})
+  }
+}
