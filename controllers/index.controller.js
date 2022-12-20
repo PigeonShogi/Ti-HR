8cc2b24f9adad8efa053271ae2348ca8d3553104
Ckai <ckai.trans@gmail.com>
2022-12-20 13:47:24 +0800

    feat: add password encryption function step 2

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -6,36 +6,31 @@ module.exports = {
   // POST /api/employees/signIn 員工可以登入系統
   signIn: async (req, res, next) => {
     const { employeeCode, password } = req.body
-    // 稍後要利用變數 checkCode 確認員工編號是否存在
-    const checkCode = await Employee.findOne({
-      where: { code: employeeCode },
-      raw: true
-    })
-    // 找出員工編號及密碼匹配正確的員工資料，此變數也用於發行 JWT。
+    // 根據使用者輸入的員工編號找尋員工資料
     const employeeData = await Employee.findOne({
-      where: { code: employeeCode, password },
-      attributes: { exclude: ['password', 'createdAt', 'updatedAt'] },
+      where: { code: employeeCode },
+      attributes: { exclude: ['createdAt', 'updatedAt'] },
       raw: true
     })
     try {
       // 檢查員工編號是否存在
-      if (!checkCode) {
+      if (!employeeData) {
         const err = new Error('你輸入的員工編號並不存在')
         err.status = 401
         throw err
       }
       // 檢查密碼錯誤次數是否達五次以上
-      if (checkCode.typoCount >= 5) {
+      if (employeeData.typoCount >= 5) {
         const err = new Error('密碼輸入錯誤累計5次以上，系統拒絕你的登入請求。請向人資同仁尋求協助。')
         err.status = 401
         throw err
       }
       // 檢查密碼是否輸入正確
-      if (!employeeData) {
+      if (!bcrypt.compareSync(password, employeeData.password)) {
         await Employee.update(
-          { typoCount: ++checkCode.typoCount },
+          { typoCount: ++employeeData.typoCount },
           { where: { code: employeeCode } })
-        const err = new Error(`密碼錯誤，累計錯誤次數${checkCode.typoCount}次。累計錯誤5次將導致無法登入，請小心。`)
+        const err = new Error(`密碼錯誤，累計錯誤次數${employeeData.typoCount}次。累計錯誤5次將導致無法登入，請小心。`)
         err.status = 401
         throw err
       }
@@ -44,6 +39,8 @@ module.exports = {
         { typoCount: 0 },
         { where: { code: employeeCode } }
       )
+      // 稍後要發行 JWT，在此先刪除 employeeData 的 password 屬性。
+      delete employeeData.password
       const token = jwt.sign(employeeData, process.env.JWT_SECRET, { expiresIn: '23h' })
       res.status(200).json({
         status: 200,
