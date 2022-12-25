2204e6971a3fc59ce6fa338ab9ef46a587c8be02
Ckai <ckai.trans@gmail.com>
2022-12-25 09:16:25 +0800

    feat: add cors setting

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 require('dotenv').config()
+
 const express = require('express')
-const app = express()
-const PORT = process.env.PORT || 8000
+const cors = require('cors')
 const router = require('./routes/index')
 
+const app = express()
+const corsOptions = {
+  origin: [
+    'https://ti-hr.up.railway.app/',
+    'http://localhost:5173',
+    'http://localhost:5174'
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'HEAD', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['content-type', 'authorization'],
+  preflightContinue: true
+}
+const PORT = process.env.PORT || 8000
+
+app.use(cors(corsOptions))
 app.use(express.urlencoded({ extended: true }))
 app.use(router)
 
