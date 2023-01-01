bc9375b27d5a37423a04578cfe146dfbbf01e5e4
Ckai <ckai.trans@gmail.com>
2023-01-01 13:34:21 +0800

    config: change cors settings

--- app.js
+++ app.js
@@ -7,15 +7,13 @@ const router = require('./routes/index')
 const app = express()
 const corsOptions = {
   origin: [
-    'https://ti-hr.up.railway.app/',
+    'https://ti-hr-client.vercel.app/',
+    'https://ti-hr-client-pigeonshogi.vercel.app/',
     'http://localhost:5173',
     'http://localhost:5174'
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'HEAD', 'PATCH', 'OPTIONS'],
-  allowedHeaders: [
-    'Content-Type',
-    'Authorization'
-  ],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
   preflightContinue: false,
   optionsSuccessStatus: 200
