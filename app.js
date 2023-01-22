4b64e330f7ab813e37f5d49eec09d0eeac809eff
Ckai <ckai.trans@gmail.com>
2023-01-22 23:04:47 +0800

    feat: add socket.io

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const router = require('./routes/index')
+const server = require('http').createServer()
 
 const app = express()
 const corsOptions = {
@@ -18,7 +19,19 @@ const corsOptions = {
   preflightContinue: false,
   optionsSuccessStatus: 200
 }
+const io = require('socket.io')(server, {
+  cors: {
+    origin: [
+      'https://ti-hr-client.vercel.app',
+      'https://ti-hr-client-pigeonshogi.vercel.app',
+      'http://localhost:5173',
+      'http://localhost:5174'
+    ],
+    methods: ['GET', 'POST']
+  }
+})
 const PORT = process.env.PORT || 8000
+const PORT_IO = process.env.PORT_IO || 3030
 // 引用排程
 const job = require('./tools/schedule')
 
@@ -30,4 +43,12 @@ job.start()
 
 app.listen(PORT, () => {
   console.info(`Listening on port ${PORT}...`)
+})
+
+server.listen(PORT_IO, () => {
+  console.info(`Listening on port ${PORT_IO} (socket.io)`)
+})
+
+io.on('connection', (socket) => {
+  console.info(`A user connected. ID: ${socket.id}`)
 })
